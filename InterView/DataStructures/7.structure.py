8f938b7671d416d00fdf4518525cfc49abd93cd6
apang <1305999617@qq.com>
2020-06-17 10:15:35 +0800

    'mysql/Redis知识点总结，A、B的最小公倍数求解'

---
+++ InterView/DataStructures/7.structure.py
@@ -0,0 +1,29 @@
+'''
+题目描述
+正整数A和正整数B 的最小公倍数是指 能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。
+'''
+
+# def cheng(A, B):
+    
+#     if A == B:
+
+#         return A
+
+#     listA = [A*i for i in range(1, B+1)]
+#     listB = [B*i for i in range(1, A+1)]
+#     # listA 和 listB 的并集
+#     a = list(set(listA)&set(listB))         
+#     a.sort()
+    
+#     return a[0]
+
+# print(cheng(4, 8))
+
+
+
+
+
+
+    
+
+
